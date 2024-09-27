b18e3a9470a2e0ffb9050ee84f0a79ae9f5f412b
Jeff Handley <jeffhandley@users.noreply.github.com>
2024-09-27 09:56:45 -0700

    Create the console app monitor that compares against a baseline

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UtilityCli;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var url = "https://www.eastrailflatswoodinville.com";
+
+        var cli = CliArgs.Parse(args);
+        bool save = cli.GetBoolean("save") ?? false;
+        string baselineFile = cli.GetString() ?? "baseline.md";
+
+        string actions = ExtractPageActions(url);
+
+        if (File.Exists(baselineFile))
+        {
+            string baseline = File.ReadAllText(baselineFile);
+
+            if (baseline != actions)
+            {
+                Console.WriteLine("DIFFERENCE FOUND!");
+                Console.WriteLine();
+                Console.WriteLine(actions);
+                Console.WriteLine();
+
+                if (!save)
+                {
+                    Console.WriteLine($"Run again using `-s|--save` to update {baselineFile} with the new results.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("NO DIFFERENCES FROM BASELINE");
+                Console.WriteLine();
+                Console.WriteLine(actions);
+            }
+        }
+
+        if (save)
+        {
+            File.WriteAllText(baselineFile, actions);
+            Console.WriteLine($"Baseline file {baselineFile} updated with results.");
+        }
+    }
+
+    static string ExtractPageActions(string url)
+    {
+        var web = new HtmlWeb();
+        var doc = web.Load(url);
+        var actions = new StringBuilder();
+
+        // Extract all links
+        var links = doc.DocumentNode.SelectNodes("//a[@href]");
+
+        if (links.Any())
+        {
+            actions.AppendLine("## LINKS");
+            actions.AppendLine();
+
+            foreach (var link in links)
+            {
+                string linkText = link.InnerText;
+
+                if (string.IsNullOrWhiteSpace(linkText))
+                {
+                    var img = link.SelectNodes("img[@alt]").FirstOrDefault();
+                    if (img is not null)
+                    {
+                        linkText = img.Attributes["alt"].Value;
+                    }
+                }
+
+                actions.AppendLine($"- {linkText}:\n  - {link.Attributes["href"].Value}");
+            }
+        }
+
+        // Extract all buttons
+        var buttons = doc.DocumentNode.SelectNodes("//button");
+
+        if (buttons.Any())
+        {
+            actions.AppendLine();
+            actions.AppendLine("## BUTTONS");
+            actions.AppendLine();
+
+            foreach (var button in buttons)
+            {
+                actions.AppendLine($"- {button.InnerText}");
+            }
+        }
+
+        return actions.ToString();
+    }
+}
---
+++ baseline.md
@@ -0,0 +1,32 @@
+## LINKS
+
+- Retail Inquiry:
+  - #contact
+- Eastrail Flats:
+  - /
+- View Location:
+  - https://www.google.com/maps/place/13300+135th+Ave+NE,+Woodinville,+WA+98072/@47.7548397,-122.1633823,17.2z/data=!4m6!3m5!1s0x54900c2bcf653447:0x2d976f4a594b129b!8m2!3d47.754629!4d-122.1614552!16s%2Fg%2F11c1z66_qz
+- Retail Brochure:
+  - https://www.mspgroupllc.com/assets/docs/Eastrail%20Flats%20-%20Retail%20Opportunity.pdf
+- View Live Camera:
+  - /webcam/
+- Explore The Schoolhouse District:
+  - https://www.theschoolhousedistrict.com/
+- (425) 822 - 5522:
+  - tel: (425) 822 - 5522
+- aoliveri@fwp-inc.com:
+  - mailto:aoliveri@fwp-inc.com
+- Retail Brochure:
+  - https://www.mspgroupllc.com/assets/docs/Eastrail%20Flats%20-%20Retail%20Opportunity.pdf
+- Porch + Park Redmond Apartments:
+  - https://porchandparkredmond.com/
+- District Flats Woodinville Apartments:
+  - https://districtflatswoodinville.com/
+- 13400 NE 175th St - Woodinville, WA:
+  - https://www.google.com/maps/place/13300+135th+Ave+NE,+Woodinville,+WA+98072/@47.7548397,-122.1633823,17.2z/data=!4m6!3m5!1s0x54900c2bcf653447:0x2d976f4a594b129b!8m2!3d47.754629!4d-122.1614552!16s%2Fg%2F11c1z66_qz
+- MainStreet Property Group LLC:
+  - https://www.mspgroupllc.com/
+
+## BUTTONS
+
+- Submit Message
