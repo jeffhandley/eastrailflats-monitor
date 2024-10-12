a2b76e2e598b3a5be454f226946ef8665a4f482e
Jeff Handley <jeffhandley@users.noreply.github.com>
2024-10-12 14:27:21 -0700

    Use Twilio to send notifications. Run with a GitHub workflow. (#1)
    * Send notifications via Twilio
    
    * Rename solution file for consistency
    
    * Add GitHub workflow to run the monitor
    
    * Use .NET 9 for the GitHub workflow

--- Program.cs
+++ Program.cs
@@ -1,42 +1,53 @@
-﻿using HtmlAgilityPack;
-using System;
+﻿using System;
 using System.IO;
 using System.Linq;
 using System.Text;
+using HtmlAgilityPack;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 using UtilityCli;
 
 class Program
 {
+    const string EastrailFlatsUrl = "https://www.eastrailflatswoodinville.com";
+
     static void Main(string[] args)
     {
-        var url = "https://www.eastrailflatswoodinville.com";
-
         var cli = CliArgs.Parse(args);
         bool save = cli.GetBoolean("save") ?? false;
+
+        string? twilioAccountSid = cli.GetString("twilio-account-sid");
+        string? twilioAuthToken = cli.GetString("twilio-auth-token");
+        string? notificationTo = cli.GetString("notification-to", ["to"]);
+        string? notificationFrom = cli.GetString("notification-from", ["from"]);
+
         string baselineFile = cli.GetString() ?? "baseline.md";
 
-        string actions = ExtractPageActions(url).ReplaceLineEndings("\n");
+        string actions = ExtractPageActions(EastrailFlatsUrl).ReplaceLineEndings("\n");
         Console.WriteLine(actions);
 
-        if (File.Exists(baselineFile))
+        bool hasDifferences = HasDifferences(baselineFile, actions);
+
+        if (hasDifferences)
         {
-            string baseline = File.ReadAllText(baselineFile).ReplaceLineEndings("\n");
+            Console.WriteLine();
+            Console.WriteLine("DIFFERENCE FOUND!");
 
-            if (baseline != actions)
+            if (!save)
             {
-                Console.WriteLine();
-                Console.WriteLine("DIFFERENCE FOUND!");
+                Console.WriteLine($"Run again using `-s|--save` to update {baselineFile} with the new results.");
+            }
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("NO DIFFERENCES FROM BASELINE");
+        }
 
-                if (!save)
-                {
-                    Console.WriteLine($"Run again using `-s|--save` to update {baselineFile} with the new results.");
-                }
-            }
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine("NO DIFFERENCES FROM BASELINE");
-            }
+        if (twilioAccountSid is not null && twilioAuthToken is not null && notificationTo is not null && notificationFrom is not null)
+        {
+            SendNotification(hasDifferences, twilioAccountSid, twilioAuthToken, notificationTo, notificationFrom);
         }
 
         if (save)
@@ -44,6 +55,36 @@ class Program
             File.WriteAllText(baselineFile, actions);
             Console.WriteLine($"Baseline file {baselineFile} updated with results.");
         }
+    }
+
+    static bool HasDifferences(string baselineFile, string actions)
+    {
+        if (File.Exists(baselineFile))
+        {
+            string baseline = File.ReadAllText(baselineFile).ReplaceLineEndings("\n");
+            return baseline != actions;
+        }
+
+        return true;
+    }
+
+    static void SendNotification(bool hasDifferences, string accountSid, string authToken, string notificationTo, string notificationFrom)
+    {
+        TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+        DateTime currentTime = TimeZoneInfo.ConvertTime(DateTime.Now, timeZone);
+        string formattedTime = currentTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+        TwilioClient.Init(accountSid, authToken);
+
+        string body = $"{formattedTime}\n{(hasDifferences ? $"CHANGES FOUND!\n{EastrailFlatsUrl}" : "No changes found.")}";
+
+        var message = MessageResource.Create(
+            to: new PhoneNumber(notificationTo),
+            from: new PhoneNumber(notificationFrom),
+            body: body
+        );
+
+        Console.WriteLine($"Twilio Message sent: {message.Sid}");
     }
 
     static string ExtractPageActions(string url)
